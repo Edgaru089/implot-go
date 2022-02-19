86c52fc674a4413d1f8eb933f45bc58862cca742
Edgaru089 <sjs333@outlook.com>
2022-02-19 23:24:56 +0800

    Fix linking on Windows

--- Link.go
+++ Link.go
@@ -3,6 +3,7 @@ package implot
 // #cgo CPPFLAGS: -DIMGUI_DISABLE_WIN32_DEFAULT_IME_FUNCTIONS
 // #cgo CXXFLAGS: -std=c++11
 // #cgo CXXFLAGS: -Wno-subobject-linkage
-// #cgo linux && amd64 LDFLAGS:  -L./implot -limplot -limgui
-// #cgo linux && amd64 CPPFLAGS: -DIMPLOT_GO_PREBUILT
+// #cgo linux&&amd64 LDFLAGS:  -L./implot -limplot -limgui
+// #cgo linux&&amd64 CPPFLAGS: -DIMPLOT_GO_PREBUILT
+// #cgo LDFLAGS: -Wl,--allow-multiple-definition
 import "C"
